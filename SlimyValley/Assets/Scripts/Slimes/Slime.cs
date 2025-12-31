2d3aabe7441656fcdfef9bf1919f21246a607a68
Alexis-Blouin <116613522+Alexis-Blouin@users.noreply.github.com>
2025-12-31 18:04:23 -0500

    making the slime wander around with navmesh

--- SlimyValley/Assets/Scripts/Slimes/Slime.cs
+++ SlimyValley/Assets/Scripts/Slimes/Slime.cs
@@ -1,9 +1,17 @@
 using System;
+using System.Collections;
 using UnityEngine;
+using UnityEngine.AI;
 using Random = UnityEngine.Random;
 
 public class Slime : MonoBehaviour
 {
+    [SerializeField] private float wanderRadius = 5.0f;
+    [SerializeField] private float idleTime = 2.0f;
+    
+    private NavMeshAgent _agent;
+    private bool _waiting;
+    
     private Animator _animator;
     private int _idleCount = 3;
 
@@ -14,11 +22,48 @@ public class Slime : MonoBehaviour
 
     private void Start()
     {
+        _agent = GetComponent<NavMeshAgent>();
+        _agent.updateRotation = false;
+        _agent.updateUpAxis = false;
+        
         PickNextIdle();
+    }
+
+    private void Update()
+    {
+        // _agent.SetDestination(target.position);
+        if (_waiting) return;
+
+        if (!_agent.pathPending && _agent.remainingDistance <= _agent.stoppingDistance)
+        {
+            StartCoroutine(WaitAndPickNew());
+        }
     }
 
     public void PickNextIdle()
     {
         _animator.SetInteger("IdleIndex", Random.Range(0, _idleCount));
+    }
+
+    private void PickNewDestination()
+    {
+        Vector3 randomPoint = transform.position + Random.insideUnitSphere * wanderRadius;
+
+        if (NavMesh.SamplePosition(
+                randomPoint,
+                out NavMeshHit hit,
+                wanderRadius,
+                _agent.areaMask))
+        {
+            _agent.SetDestination(hit.position);
+        }
+    }
+
+    private IEnumerator WaitAndPickNew()
+    {
+        _waiting = true;
+        yield return new WaitForSeconds(idleTime);
+        _waiting = false;
+        PickNewDestination();
     }
 }
